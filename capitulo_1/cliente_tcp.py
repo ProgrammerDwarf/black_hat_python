2c2c276876567eeffda234afa4dc1370e7e2ce1b
Reinid <reinidval@gmail.com>
2026-09-21 09:43:26 -0300

    feature: creacion de servidor tcp y ajuste de cliente tcp

--- capitulo_1/cliente_tcp.py
+++ capitulo_1/cliente_tcp.py
@@ -1,7 +1,9 @@
 import socket
 
-target_host = 'www.google.com'
-target_port = 80
+#target_host = 'www.google.com'
+target_host = '127.0.0.1'
+#target_port = 80
+target_port = 9998
 
 # Creación de objeto socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,7 +12,7 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((target_host, target_port))
 
 # Enviar datos
-client.send(b"GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n")
+client.send(b"GET / HTTP/1.1\r\nHost: 0.0.0.0\r\n\r\n")
 
 # Recibir datos
 response = client.recv(4096)
